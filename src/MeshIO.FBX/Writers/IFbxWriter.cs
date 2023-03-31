1f9d19ece71bdaab5064a77e798e4651ac072119
DomCR <DomCR@users.noreply.github.com>
2023-03-31 17:19:47 +0200

    samples

--- src/MeshIO.FBX/Writers/IFbxWriter.cs
+++ src/MeshIO.FBX/Writers/IFbxWriter.cs
@@ -2,6 +2,7 @@
 
 namespace MeshIO.FBX
 {
+	[Obsolete]
 	public interface IFbxWriter: IDisposable
 	{
 		/// <summary>
--- src/MeshIO.Tests.Shared/FolderPath.cs
+++ src/MeshIO.Tests.Shared/FolderPath.cs
@@ -7,4 +7,7 @@ public static class FolderPath
 
 	public static readonly string InFilesFbx = Path.Combine(InFiles, "fbx");
 	public static readonly string OutFilesFbx = Path.Combine(OutFiles, "fbx");
+
+	public static readonly string InFilesGltf = Path.Combine(InFiles, "glb-gltf");
+	public static readonly string OutFilesGltf = Path.Combine(OutFiles, "glb-gltf");
 }
