fa7634d6b3aca0f824daa9d5415e222bb082af55
DomCR <DomCR@users.noreply.github.com>
2023-04-01 10:01:29 +0200

    node

--- src/MeshIO/Node.cs
+++ src/MeshIO/Node.cs
@@ -14,14 +14,33 @@ namespace MeshIO
 		[Obsolete]
 		public string Culling { get; set; } = "CullingOff";
 
+		/// <summary>
+		/// Get the local transform for this node
+		/// </summary>
 		public Transform Transform { get; internal set; } = new Transform();
 
+		/// <summary>
+		/// Get the parent for this node
+		/// </summary>
 		public Element3D Parent { get; }
 
+		/// <summary>
+		/// Get all linked elements to this node
+		/// </summary>
 		public List<Element3D> Children { get; } = new List<Element3D>();
 
 		public Node() : base() { }
 
 		public Node(string name) : base(name) { }
+
+		/// <summary>
+		/// Get the global transformation for this node
+		/// </summary>
+		/// <returns></returns>
+		/// <exception cref="NotImplementedException"></exception>
+		public Transform GetGlobalTransform()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
