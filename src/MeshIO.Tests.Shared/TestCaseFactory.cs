dc6c1a5ee4e8933068cde2d4b26c98a227ca16fe
DomCR <DomCR@users.noreply.github.com>
2023-04-05 20:03:41 +0200

    note

--- src/MeshIO.Tests.Shared/TestCaseFactory.cs
+++ src/MeshIO.Tests.Shared/TestCaseFactory.cs
@@ -6,6 +6,8 @@ namespace MeshIO.Tests.Shared
 	{
 		public static Mesh CreateBox()
 		{
+			//TODO: complete test factory
+
 			Mesh mesh = new Mesh("Box");
 			mesh.Vertices.Add(new CSMath.XYZ(0, 0, 0));
 			mesh.Vertices.Add(new CSMath.XYZ(10, 0, 0));
