f552932320b4756f26c36ffe6cdec2ab8d076665
DomCR <DomCR@users.noreply.github.com>
2024-01-18 13:10:33 +0100

    writers objects

---
+++ src/MeshIO.FBX/Writers/Objects/FbxMeshWriter.cs
@@ -0,0 +1,154 @@
+﻿using CSMath;
+using MeshIO.Entities;
+using MeshIO.Entities.Geometries;
+using MeshIO.Entities.Geometries.Layers;
+using MeshIO.FBX.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MeshIO.FBX.Writers.Objects
+{
+	internal abstract class FbxEntityWriter<T> : FbxObjectWriterBase<T>
+		where T : Entity
+	{
+		public override string FbxObjectName { get; } = FbxFileToken.Geometry;
+
+		protected FbxEntityWriter(T element) : base(element)
+		{
+		}
+	}
+
+	internal class FbxMeshWriter : FbxEntityWriter<Mesh>
+	{
+		public override string FbxTypeName { get; } = FbxFileToken.Mesh;
+
+		public FbxMeshWriter(Mesh element) : base(element)
+		{
+		}
+
+		protected override void writeObjectBody(FbxFileWriterBase builder, IFbxStreamWriter writer)
+		{
+			base.writeObjectBody(builder, writer);
+
+			if (this._element.Vertices.Any())
+			{
+				double[] arr = _element.Vertices.SelectMany(x => x.ToEnumerable()).ToArray();
+				writer.WritePairNodeValue("Vertices", arr);
+				writer.WritePairNodeValue("PolygonVertexIndex", polygonsArray(this._element));
+
+				if (this._element.Edges.Any())
+				{
+					writer.WritePairNodeValue("Edges", arr);
+				}
+			}
+
+			writer.WritePairNodeValue("GeometryVersion", 124);
+
+			this.writeLayers(builder, writer);
+		}
+
+		private void writeLayers(FbxFileWriterBase builder, IFbxStreamWriter writer)
+		{
+			List<string> layers = new List<string>();
+
+			this.writeMaterial(builder, writer);
+
+			foreach (LayerElement layer in this._element.Layers)
+			{
+				this.writeLayer(layer, builder, writer);
+
+				layers.Add(layer.GetFbxName());
+			}
+
+			writer.WriteName("Layer");
+			writer.WriteValue(0);
+			writer.WriteOpenBracket();
+			writer.WritePairNodeValue(FbxFileToken.Version, 100);
+
+
+			foreach (string name in layers)
+			{
+				writer.WriteName("LayerElement");
+				writer.WriteOpenBracket();
+
+				writer.WritePairNodeValue("Type", name);
+				writer.WritePairNodeValue("TypedIndex", 0);
+
+				writer.WriteCloseBracket();
+				writer.WriteEmptyLine();
+			}
+
+			writer.WriteCloseBracket();
+			writer.WriteEmptyLine();
+		}
+
+		private void writeMaterial(FbxFileWriterBase builder, IFbxStreamWriter writer)
+		{
+			return;
+
+			if (!this._element.ParentNodes.Any())
+			{
+				return;
+			}
+		}
+
+		private void writeLayer<T>(T layer, FbxFileWriterBase builder, IFbxStreamWriter writer)
+			where T : LayerElement
+		{
+			writer.WriteName(layer.GetFbxName());
+			writer.WriteValue(0);
+			writer.WriteOpenBracket();
+			writer.WritePairNodeValue(FbxFileToken.Version, 100);
+			writer.WritePairNodeValue("Name", layer.Name);
+			writer.WritePairNodeValue("MappingInformationType", layer.MappingMode.GetFbxName());
+			writer.WritePairNodeValue("ReferenceInformationType", layer.ReferenceMode.GetFbxName());
+
+			switch (layer)
+			{
+				case LayerElementNormal normals:
+					writer.WritePairNodeValue("Normals", normals.Normals.SelectMany(x => x.ToEnumerable()).ToArray());
+					writer.WritePairNodeValue("NormalsW", normals.Weights.ToArray());
+					break;
+				case LayerElementUV uv:
+					writer.WritePairNodeValue("UV", uv.UV.SelectMany(x => x.ToEnumerable()).ToArray());
+					break;
+				default:
+					break;
+			}
+
+			writer.WriteCloseBracket();
+			writer.WriteEmptyLine();
+		}
+
+		protected int[] polygonsArray(Mesh mesh)
+		{
+			List<int> arr = new List<int>();
+
+			//Check if the polygons list is empty
+			if (!mesh.Polygons.Any())
+				return arr.ToArray();
+
+			if (mesh.Polygons.First() is Triangle)
+			{
+				foreach (Triangle t in mesh.Polygons)
+				{
+					arr.Add((int)t.Index0);
+					arr.Add((int)t.Index1);
+					arr.Add(-((int)t.Index2 + 1));
+				}
+			}
+			else
+			{
+				foreach (Quad t in mesh.Polygons)
+				{
+					arr.Add((int)t.Index0);
+					arr.Add((int)t.Index1);
+					arr.Add((int)t.Index2);
+					arr.Add(-((int)t.Index3 + 1));
+				}
+			}
+
+			return arr.ToArray();
+		}
+	}
+}
---
+++ src/MeshIO.FBX/Writers/Objects/FbxObjectWriterBase.cs
@@ -0,0 +1,105 @@
+﻿using MeshIO.Entities.Geometries;
+using System;
+using System.Collections.Generic;
+
+namespace MeshIO.FBX.Writers.Objects
+{
+	internal static class FbxObjectWriterFactory
+	{
+		public static IFbxObjectWriter Create<T>(T element)
+			where T : Element3D
+		{
+			switch (element)
+			{
+				case Mesh mesh:
+					return new FbxMeshWriter(mesh);
+				case Node node:
+					return new FbxNodeWriter(node);
+				default:
+					throw new NotImplementedException($"IFbxObjectWriter for {element.GetType()}");
+			}
+		}
+	}
+
+	internal abstract class FbxObjectWriterBase<T> : IFbxObjectWriter
+		where T : Element3D
+	{
+		public ulong Id { get { return this._element.Id.Value; } }
+
+		public string Name { get { return this._element.Name; } }
+
+		public abstract string FbxObjectName { get; }
+
+		public abstract string FbxTypeName { get; }
+
+		public Dictionary<string, FbxProperty> FbxInstanceProperties { get; } = new();
+
+		protected readonly T _element;
+
+		protected FbxObjectWriterBase(T element)
+		{
+			_element = element;
+		}
+
+		public void Write(FbxFileWriterBase builder, IFbxStreamWriter writer)
+		{
+			this.writeObjectHeader(writer);
+
+			writer.WriteOpenBracket();
+
+			this.writeObjectBody(builder, writer);
+
+			writer.WriteCloseBracket();
+			writer.WriteEmptyLine();
+		}
+
+		public virtual void ProcessChildren(FbxFileWriterBase fbxFileWriterBase)
+		{
+		}
+
+		public virtual void ApplyTemplate(FbxPropertyTemplate template)
+		{
+			foreach (Property item in this._element.Properties)
+			{
+				if (template.Properties.TryGetValue(item.Name, out FbxProperty property)
+					&& item.Value.Equals(property.Value))
+				{
+					continue;
+				}
+
+				this.FbxInstanceProperties.Add(item.Name, FbxProperty.CreateFrom(item));
+			}
+		}
+
+		protected virtual void writeObjectHeader(IFbxStreamWriter writer)
+		{
+			writer.WriteName(this.FbxObjectName);
+			writer.WriteValue(this.getId());
+			writer.WriteValue($"{this.FbxObjectName}::{_element.Name}");
+			writer.WriteValue(this.FbxTypeName);
+		}
+
+		protected virtual void writeObjectBody(FbxFileWriterBase builder, IFbxStreamWriter writer)
+		{
+			builder.WriteProperties(this.FbxInstanceProperties.Values);
+		}
+
+		protected void addProperty(FbxPropertyTemplate template, string name, object value)
+		{
+			if (template.GetUpdated(name, value, out FbxProperty p))
+			{
+				this.FbxInstanceProperties.Add(p.Name, p);
+			}
+		}
+
+		private long getId()
+		{
+			if (!_element.Id.HasValue)
+			{
+				_element.Id = IdUtils.CreateId();
+			}
+
+			return Math.Abs((long)_element.Id.Value);
+		}
+	}
+}
