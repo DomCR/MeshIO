27fc7e52c847716ad29e044cf8dd6881a4569e0e
DomCR <DomCR@users.noreply.github.com>
2024-01-18 13:02:17 +0100

    Extensions

---
+++ src/MeshIO.FBX/Extensions/Element3DExtensions.cs
@@ -0,0 +1,21 @@
+﻿using MeshIO.Entities.Geometries;
+using System;
+
+namespace MeshIO.FBX.Extensions
+{
+	internal static class Element3DExtensions
+	{
+		public static string GetFbxName(this Element3D element)
+		{
+			switch (element)
+			{
+				case Node:
+					return FbxFileToken.Model;
+				case Geometry:
+					return FbxFileToken.Geometry;
+				default:
+					throw new NotImplementedException($"Unknown Element3D : {element.GetType().FullName}");
+			}
+		}
+	}
+}
---
+++ src/MeshIO.FBX/Extensions/LayerElementExtensions.cs
@@ -0,0 +1,55 @@
+﻿using MeshIO.Entities.Geometries.Layers;
+using System;
+
+namespace MeshIO.FBX.Extensions
+{
+	internal static class LayerElementExtensions
+	{
+		public static string GetFbxName(this LayerElement layer)
+		{
+			switch (layer)
+			{
+				case LayerElementNormal:
+					return "LayerElementNormal";
+				case LayerElementUV:
+					return "LayerElementUV";
+				default:
+					throw new NotImplementedException($"Unknown LayerElement Type : {layer.GetType().FullName}");
+			}
+		}
+
+		public static string GetFbxName(this MappingMode mappingMode)
+		{
+			switch (mappingMode)
+			{
+				case MappingMode.ByVertex:
+					return "ByVertice";
+				case MappingMode.ByPolygonVertex:
+					return "ByPolygonVertex";
+				case MappingMode.ByPolygon:
+					return "ByPolygon";
+				case MappingMode.ByEdge:
+					return "ByEdge";
+				case MappingMode.AllSame:
+					return "AllSame";
+				default:
+					throw new ArgumentException($"Unknown MappingMode : {mappingMode}", nameof(mappingMode));
+			}
+		}
+
+		public static string GetFbxName(this ReferenceMode referenceMode)
+		{
+			switch (referenceMode)
+			{
+				case ReferenceMode.Direct:
+					return "Direct";
+				case ReferenceMode.Index:
+					return "Index";
+				case ReferenceMode.IndexToDirect:
+					return "IndexToDirect";
+				default:
+					throw new ArgumentException($"Unknown ReferenceMode : {referenceMode}", nameof(referenceMode));
+			}
+		}
+	}
+}
