8ab6df235a6ad4f9b20405fafc448af31b0c448a
DomCR <DomCR@users.noreply.github.com>
2024-01-18 13:03:34 +0100

    Fbx connections

---
+++ src/MeshIO.FBX/Writers/Connections/FbxConnection.cs
@@ -0,0 +1,24 @@
+﻿using MeshIO.FBX.Writers.Objects;
+
+namespace MeshIO.FBX.Writers.Connections
+{
+	internal class FbxConnection
+	{
+		public FbxConnectionType ConnectionType { get; }
+
+		public IFbxObjectWriter Child { get; }
+
+		public IFbxObjectWriter Parent { get; }
+
+		public FbxConnection(IFbxObjectWriter child, IFbxObjectWriter parent)
+		{
+			this.Child = child;
+			this.Parent = parent;
+		}
+
+		public string GetComment()
+		{
+			return $"{Child.FbxObjectName}::{Child.Name}, {Parent.FbxObjectName}::{Parent.Name}";
+		}
+	}
+}
