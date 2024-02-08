9c843e49ec735586b169844732ea9825f1d18a3c
DomCR <DomCR@users.noreply.github.com>
2024-02-08 12:53:26 +0100

    obsolete

--- src/MeshIO.FBX/FbxProperty.cs
+++ src/MeshIO.FBX/FbxProperty.cs
@@ -5,47 +5,6 @@ using System.Linq;
 
 namespace MeshIO.FBX
 {
-	//P : ["PropName", "PropType", "Label(?)", "Flags", __values__, …]
-	[Obsolete]
-	public class FbxPropertyOld : Property
-	{
-		//Model porperties
-		public const string LclTranslation = "Lcl Translation";
-		public const string LclScaling = "Lcl Scaling";
-		public const string LclRotation = "Lcl Rotation";
-
-		public const string CastShadows = "Casts Shadows";
-		public const string ReceiveShadows = "Receive Shadows";
-		public const string PrimaryVisibility = "Primary Visibility";
-		public const string Show = "Show";  //IsVisible
-		public const string Freeze = "Freeze";  //
-
-		//Material Properties
-		public const string AmbientColor = "AmbientColor";
-
-		public static readonly System.Text.RegularExpressions.Regex PropertiesRegex = new System.Text.RegularExpressions.Regex(@"(Properties).*?[\d]+");
-
-		public string FbxTypeName { get; set; }
-
-		public string TypeLabel { get; set; }
-
-		public FbxPropertyOld(string name, Element3D owner) : base(name, owner) { }
-
-		public FbxPropertyOld(string name, Element3D owner, object value) : base(name, owner, value) { }
-
-		public FbxPropertyOld(string name, Element3D owner, object value, string typeName, string typeLabel, PropertyFlags flags) : base(name, owner, value)
-		{
-			this.FbxTypeName = typeName;
-			this.TypeLabel = typeLabel;
-			this.Flags = flags;
-		}
-	}
-
-	public class FbxProperty<T> : FbxProperty
-	{
-		public FbxProperty(string name, PropertyFlags flags, T value) : base(name, flags, value) { }
-	}
-
 	public class FbxProperty : Property
 	{
 		public static class Geometry
