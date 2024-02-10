4450c7e21af2a33557bc860a39e64b328797b971
DomCR <DomCR@users.noreply.github.com>
2024-02-10 11:31:19 +0100

    node

--- src/MeshIO/Node.cs
+++ src/MeshIO/Node.cs
@@ -2,9 +2,13 @@
 using MeshIO.Shaders;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MeshIO
 {
+	/// <summary>
+	/// Scene node
+	/// </summary>
 	public class Node : SceneElement
 	{
 		/// <summary>
@@ -22,15 +26,74 @@ namespace MeshIO
 		/// </summary>
 		public Element3D Parent { get; }
 
+		/// <summary>
+		/// Nested nodes
+		/// </summary>
 		public List<Node> Nodes { get; } = new();
 
+		/// <summary>
+		/// Gets the default material for this node
+		/// </summary>
+		public Material Material
+		{
+			get
+			{
+				return this.Materials.FirstOrDefault();
+			}
+		}
+
+		/// <summary>
+		/// Materials attached to this node
+		/// </summary>
 		public List<Material> Materials { get; } = new();
 
+		/// <summary>
+		/// Gets the defining entity for this node
+		/// </summary>
+		public Entity Entity
+		{
+			get
+			{
+				return this.Entities.FirstOrDefault();
+			}
+		}
+
+		/// <summary>
+		/// Entities attached to this node
+		/// </summary>
 		public List<Entity> Entities { get; } = new();
 
-		public Node() : base() { }
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public Node() : this(string.Empty) { }
 
 		public Node(string name) : base(name) { }
+
+		/// <summary>
+		/// Add an <see cref="Element3D"/> to this node structure
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="element"></param>
+		/// <exception cref="ArgumentException"></exception>
+		public void Add<T>(T element)
+			where T : Element3D
+		{
+			switch (element)
+			{
+				case Entity e:
+					this.Entities.Add(e);
+					break;
+				case Material m:
+					this.Materials.Add(m);
+					break;
+				case Node n:
+					this.Nodes.Add(n);
+					break;
+				default:
+					throw new ArgumentException("", nameof(element));
+			}
+		}
 
 		/// <summary>
 		/// Get the global transformation for this node
