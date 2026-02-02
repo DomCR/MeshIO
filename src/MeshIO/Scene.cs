b297ba9bdcd7afdaa44ab38f61521729ccde4a92
DomCR <domenech93@gmail.com>
2026-02-02 13:55:30 +0100

    docs

--- src/MeshIO/Scene.cs
+++ src/MeshIO/Scene.cs
@@ -2,18 +2,30 @@
 
 namespace MeshIO;
 
+/// <summary>
+/// Represents a scene containing a hierarchical structure of nodes and <see cref="SubScenes"/>.
+/// </summary>
+/// <remarks>A scene serves as the root container for a collection of nodes, typically representing objects or
+/// elements within a graphical or logical environment. Scenes can be composed hierarchically by including <see cref="SubScenes"/>,
+/// allowing for modular organization and reuse. The root node provides the entry point to the node tree for this
+/// scene.</remarks>
 public class Scene : SceneElement
 {
 	/// <summary>
-	/// Root node of the scene
+	/// Gets the root node of the tree structure.
 	/// </summary>
-	public Node RootNode { get; } = new Node();
+	public Node RootNode { get; } = new Node();//TODO: Should it be a class for this one? (only nodes allowed)
 
+	/// <summary>
+	/// Gets the collection of sub-scenes contained within this scene.
+	/// </summary>
 	public List<Scene> SubScenes { get; } = new List<Scene>();
 
-	private readonly Dictionary<ulong, Element3D> _elements = new Dictionary<ulong, Element3D>();
+	public Scene() : base()
+	{
+	}
 
-	public Scene() : base() { }
-
-	public Scene(string name) : base(name) { }
+	public Scene(string name) : base(name)
+	{
+	}
 }
--- src/MeshIO/SceneElement.cs
+++ src/MeshIO/SceneElement.cs
@@ -1,13 +1,26 @@
 ﻿namespace MeshIO;
 
-public abstract class SceneElement : Element3D
+/// <summary>
+/// Represents a 3D element that is part of a scene.
+/// </summary>
+/// <remarks>SceneElement serves as a base class for objects that are contained within a Scene. It provides access
+/// to the associated Scene and inherits from Element3D. Derived classes should implement specific behaviors or visual
+/// representations within the scene.</remarks>
+public abstract class SceneElement : Element3D	//TODO: is it needed? scene never assigned
 {
 	/// <summary>
-	/// Scene where this element belongs to
+	/// Gets the scene associated with this instance.
 	/// </summary>
 	public Scene Scene { get; }
 
+	/// <summary>
+	/// Initializes a new instance of the SceneElement class.
+	/// </summary>
 	public SceneElement() : base() { }
 
+	/// <summary>
+	/// Initializes a new instance of the SceneElement class with the specified name.
+	/// </summary>
+	/// <param name="name">The name to assign to the scene element.</param>
 	public SceneElement(string name) : base(name) { }
 }
