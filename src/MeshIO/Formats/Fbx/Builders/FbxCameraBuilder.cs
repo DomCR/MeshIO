67584bd3c551909a1125512ecab70a156c78533e
DomCR <domenech93@gmail.com>
2026-02-02 13:50:05 +0100

    fbx

--- src/MeshIO/Formats/Fbx/Builders/FbxCameraBuilder.cs
+++ src/MeshIO/Formats/Fbx/Builders/FbxCameraBuilder.cs
@@ -3,6 +3,7 @@ using CSUtilities.Extensions;
 using MeshIO.Entities;
 using MeshIO.Formats.Fbx.Readers;
 using System;
+using System.Collections.Generic;
 
 namespace MeshIO.Formats.Fbx.Builders;
 
@@ -19,6 +20,31 @@ internal class FbxCameraBuilder : FbxObjectBuilder<Camera>
 	public override void Build(FbxFileBuilderBase builder)
 	{
 		base.Build(builder);
+	}
+
+	protected override void buildProperties(Dictionary<string, FbxProperty> properties)
+	{
+		if (properties.Remove("NearPlane", out FbxProperty nearPlane))
+		{
+			_element.NearPlane = (double)nearPlane.ToProperty().Value;
+		}
+
+		if (properties.Remove("FarPlane", out FbxProperty farPlane))
+		{
+			_element.FarPlane = (double)farPlane.ToProperty().Value;
+		}
+
+		if (properties.Remove("FieldOfView", out FbxProperty fieldOfView))
+		{
+			_element.FieldOfView = (double)fieldOfView.ToProperty().Value;
+		}
+
+		if (properties.Remove("OrthoZoom", out FbxProperty orthoZoom))
+		{
+			_element.OrtographicZoom = new XY((double)orthoZoom.ToProperty().Value);
+		}
+
+		base.buildProperties(properties);
 	}
 
 	protected override bool setValue(FbxFileBuilderBase builder, FbxNode node)
